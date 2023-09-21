e2842690a782d6135923ab379e3b4137a1f71c3d
Harrys Sidiropoulos <105165515+HrsSid@users.noreply.github.com>
2023-09-21 22:46:17 +0300

    Create requirements.py

---
+++ requirements.py
@@ -0,0 +1,8 @@
+import os
+os.system("title WHOOX - Installing requirements")
+
+# Installing the requirements
+os.system("pip install platform")
+os.system("pip install phonenumbers")
+os.system("pip install requests")
+os.system("pip install csv")
